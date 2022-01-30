24c11f8e778e88e3969d3b778e6981862367451d
alisher <alisherkabildjanov@yahoo.com>
2022-01-30 15:31:31 -0600

    feat: completed generation of typed graphql queries from contentful and adapted to useNewQuery

---
+++ app/components/sidebar/News.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import { NewsQuery } from 'generated/graphql';
+import { Heading, Text, Box } from '@chakra-ui/react';
+
+interface Props {
+  data: NewsQuery;
+}
+
+export const News: FC<Props> = ({ data }) => {
+  return (
+    <>
+      {data.newsCollection.items.map((news) => {
+        return (
+          <Box key={news.sys.id} mb={5}>
+            <Heading fontWeight={700} fontSize={20}>
+              {news.title}
+            </Heading>
+            <Text fontWeight={400} fontSize={12}>
+              {news.content}
+            </Text>
+          </Box>
+        );
+      })}
+    </>
+  );
+};
--- app/components/sidebar/RightSidebar.tsx
+++ app/components/sidebar/RightSidebar.tsx
@@ -2,8 +2,18 @@ import { FC } from 'react';
 import { Box, Flex, Text } from '@chakra-ui/react';
 import { colors } from 'theme/colors';
 import { VoteCard } from '../vote-card/VoteCard';
+import { useNewsQuery } from 'generated/graphql';
+import { News } from './News';
 
 export const RightSidebar: FC = () => {
+  const [result] = useNewsQuery({ requestPolicy: 'cache-and-network' });
+
+  const { fetching, data } = result;
+
+  if (fetching) {
+    return <Text>...Loading</Text>;
+  }
+
   return (
     <Flex flexDir="column" w={450} h="100vh" p={10}>
       <Box>
@@ -16,6 +26,7 @@ export const RightSidebar: FC = () => {
         <Text color={colors.primaryGray} fontWeight={700} fontSize={12}>
           NEWS & UPDATES
         </Text>
+        <News data={data} />
       </Box>
     </Flex>
   );
